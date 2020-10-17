07ca82f05297f9eaae94b0b6136aa715850de7bb
Rafael Paes <rfaelpaes@gmail.com>
2020-10-17 20:54:20 -0300

    refactor: acrescenta rotas de cadastro e detalhes

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Landing from '~/pages/Landing'
 import OrphanagesMap from '~/pages/OrphanagesMap'
+import Orphanages from '~/pages/Orphanages'
+import CreateOrphanage from '~/pages/CreateOrphanage'
 
 function Routes() {
   return (
@@ -11,6 +13,8 @@ function Routes() {
       <Switch>
         <Route path="/" component={Landing} exact />
         <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanages} />
       </Switch>
     </BrowserRouter>
   )
