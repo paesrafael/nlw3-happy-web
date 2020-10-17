66f1af0ac4ee7d9e41c263920d5cbe5423b408aa
Rafael Paes <rfaelpaes@gmail.com>
2020-10-17 20:53:31 -0300

    refactor: move o leaflet para o render do app

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 import GlobalStyles from './assets/styles/GlobalStyles'
+import 'leaflet/dist/leaflet.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    
+
     <App />
   </React.StrictMode>,
   document.getElementById('root')
